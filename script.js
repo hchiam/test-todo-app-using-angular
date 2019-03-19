7a78126d20e6607c060d3421073577595c7875e4
Howard T. Chiam <hchiam@users.noreply.github.com>
2019-03-18 23:56:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+  $scope.newToDo = '';
+  $scope.todos = [];
+  $scope.todosTemp = [];
+  $scope.add = function(todo) {
+    if (todo) {
+      $scope.todosTemp.push(todo);
+      $scope.todos = angular.copy($scope.todosTemp);
+      $scope.newToDo = '';
+    }
+  };
+  $scope.edit = function(todo, index) {
+    $scope.todosTemp[index] = todo;
+    document.getElementById("todo-" + index).focus();
+  };
+  $scope.delete = function(index) {
+    $scope.todosTemp.splice(index, 1);
+    $scope.todos = angular.copy($scope.todosTemp);
+  };
+});
