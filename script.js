3e32f3876789430dd2fdbd7f2aefd597718b1258
hchiam <hchiam@users.noreply.github.com>
2019-03-20 23:56:23 -0400

    Revert "local storage cookies working"
    This reverts commit 9e8ce77eebbd013c92dd045739dcc47a8c7eda5c.

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', ['$scope', '$window', function($scope, $window) {
+app.controller('myCtrl', function($scope) {
   $scope.newToDo = '';
   $scope.todos = [];
   $scope.todosTemp = [];
-  $scope.init = function() {
-    var haveCookieList = $window.localStorage.getItem('todos') != undefined;
-    if (haveCookieList) {
-      $scope.todos = JSON.parse($window.localStorage.getItem('todos'));
-      $scope.todosTemp = JSON.parse($window.localStorage.getItem('todos'));
-    } else {
-      $scope.todos = [];
-      $scope.todosTemp = [];
-      $window.localStorage.setItem('todos', JSON.stringify([]));
-    }
-  };
-  $scope.init();
   $scope.add = function(todo) {
     if (todo) {
       $scope.todosTemp.push(todo);
       $scope.todos = angular.copy($scope.todosTemp);
-      $window.localStorage.setItem('todos', JSON.stringify($scope.todos));
       $scope.newToDo = '';
     }
   };
@@ -30,6 +17,5 @@ app.controller('myCtrl', ['$scope', '$window', function($scope, $window) {
   $scope.delete = function(index) {
     $scope.todosTemp.splice(index, 1);
     $scope.todos = angular.copy($scope.todosTemp);
-    $window.localStorage.setItem('todos', JSON.stringify($scope.todos));
   };
-}]);
+});
